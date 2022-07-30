bda4f2048317ac2eb5693c9718e5371442a840fd
uboar <uboaruboar@gmail.com>
2022-07-30 22:13:45 +0900

    rollupOptionsを追加

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,17 @@ export default defineConfig({
     include: ['marked']
   },
   build: {
+    rollupOptions: {
+      external:[
+        'vue-material-design-icons/Youtube.vue',
+        'vue-material-design-icons/TelevisionClassic.vue',
+        'vue-material-design-icons/Twitter.vue',
+        'vue-material-design-icons/Post.vue',
+        'vue-material-design-icons/Soundcloud.vue',
+        'vue-material-design-icons/Album.vue',
+        'vue-material-design-icons/AccountCircle.vue',
+      ],
+    },
     commonjsOptions: {
       include: ['marked']
     }
