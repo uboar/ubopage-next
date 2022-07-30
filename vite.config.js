c591f719de3773ef3892bd683ad49435886cd69c
uboar <uboaruboar@gmail.com>
2022-07-30 22:18:54 +0900

    rollup修正

--- vite.config.js
+++ vite.config.js
@@ -10,19 +10,19 @@ export default defineConfig({
   build: {
     rollupOptions: {
       external:[
-        'vue-material-design-icons/Youtube.vue',
-        'vue-material-design-icons/TelevisionClassic.vue',
-        'vue-material-design-icons/Twitter.vue',
-        'vue-material-design-icons/Post.vue',
-        'vue-material-design-icons/Soundcloud.vue',
-        'vue-material-design-icons/Album.vue',
-        'vue-material-design-icons/AccountCircle.vue',
-        'vue-material-design-icons/Play.vue',
-        'vue-material-design-icons/Pause.vue',
-        'vue-material-design-icons/InformationOutline.vue',
-        'vue-material-design-icons/chevronLeft.vue',
-        'vue-material-design-icons/chevronDown.vue',
-        'vue-kinesis'
+        './vue-material-design-icons/Youtube.vue',
+        './vue-material-design-icons/TelevisionClassic.vue',
+        './vue-material-design-icons/Twitter.vue',
+        './vue-material-design-icons/Post.vue',
+        './vue-material-design-icons/Soundcloud.vue',
+        './vue-material-design-icons/Album.vue',
+        './vue-material-design-icons/AccountCircle.vue',
+        './vue-material-design-icons/Play.vue',
+        './vue-material-design-icons/Pause.vue',
+        './vue-material-design-icons/InformationOutline.vue',
+        './vue-material-design-icons/chevronLeft.vue',
+        './vue-material-design-icons/chevronDown.vue',
+        './vue-kinesis'
       ],
     },
     commonjsOptions: {
